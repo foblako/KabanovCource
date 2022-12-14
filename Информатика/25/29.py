1580ec03cc2d701b0db509f7875c38a8be38043d
foblako <113190840+foblako@users.noreply.github.com>
2022-12-14 10:41:24 +0300

    14.12.22 10.41

--- Информатика/25/29.py
+++ Информатика/25/29.py
@@ -1,5 +1,8 @@
 def prime(x):
     return x > 1 and all(x % i != 0 for i in range(2, int(x ** 0.5) + 1))
 
-
-for i in range(113000000, 114000001):
+for i in range(113_000_000, 114_000_001):
+    if i % 2 == 0 and i % 4 != 0:
+        t = i // 2
+        if int(t**0.5)**2 == t and prime(int(t**0.5)):
+            print(i, int(t**0.5)*2)
